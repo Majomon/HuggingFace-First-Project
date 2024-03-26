dba75f18e605802db45ed123dc9f9e040a4190e9
Mauri <mauri.monzon91@gmail.com>
2024-03-26 20:35:00 -0300

    De imagen a texto y tambien traductor

--- index.js
+++ index.js
@@ -3,16 +3,38 @@ import { config } from "dotenv";
 config();
 
 const hf = new HfInference(process.env.HF_ACCESS_TOKEN);
+
+/* Para imagenes */
 const imageUrl =
   "https://th-thumbnailer.cdn-si-edu.com/VCycQRxu4QwTvPAUBNYSghlgwks=/fit-in/1600x0/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/Innovations-Dogs-Furry-Kids.jpg";
 
-const model = "Salesforce/blip-image-captioning-large";
-const response = await fetch(imageUrl);
-const blob = await response.blob();
+const modelImg = "Salesforce/blip-image-captioning-large";
 
-const result = await hf.imageToText({
-  data: blob,
-  model,
-});
+try {
+  const response = await fetch(imageUrl);
+  const blob = await response.blob();
 
-console.log(result);
+  const result = await hf.imageToText({
+    data: blob,
+    model: modelImg,
+  });
+  console.log(result);
+} catch (error) {
+  console.log(error);
+}
+
+const modelTranslate = "facebook/nllb-200-distilled-600M";
+
+try {
+  const result = await hf.translation({
+    model: modelTranslate,
+    inputs: "Hello world",
+    parameters: {
+      src_lang: "en",
+      tgt_lang: "es",
+    },
+  });
+  console.log(result);
+} catch (error) {
+  console.log(error);
+}
