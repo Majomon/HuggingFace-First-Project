ea5cd3de5c7e2d5b9d7113b6eeab6b815d8a4e62
Mauri <mauri.monzon91@gmail.com>
2024-03-26 20:26:42 -0300

    Creado .env

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { HfInference } from "@huggingface/inference";
+import { config } from "dotenv";
+config();
 
-const hf = new HfInference("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");
+const hf = new HfInference(process.env.HF_ACCESS_TOKEN);
 const imageUrl =
   "https://th-thumbnailer.cdn-si-edu.com/VCycQRxu4QwTvPAUBNYSghlgwks=/fit-in/1600x0/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/Innovations-Dogs-Furry-Kids.jpg";
 
